32a33dfc77c64596c0a529c80045c1d8236e3e6d
igorgp06 <igorgoncalvesp20@gmail.com>
2026-06-06 20:40:24 -0300

    [WORK 1.0.2] Adição do Background com a animação das estrelas e meteoros.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,13 @@
+import { Background } from "../components/utils/Background.jsx"
+
 import { Navbar } from "../components/utils/Navbar"
 
 import { Hero } from "./public/partials/hero/Hero"
 
 export const Main = () => {
     return (
-        <div>
-
+        <div className="min-h-screen bg-background overflow-x-hidden">
+            <Background />
             <Navbar />
 
             <main>
